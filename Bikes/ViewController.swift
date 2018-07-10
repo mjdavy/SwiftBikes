453b455d6461d2651cc57cc9b4888a7082ab311e
Martin Davy <mjdavy@hotmail.com>
2018-07-10 10:24:07 -0400

    Initial Commit

---
+++ Bikes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bikes
+//
+//  Created by Martin Davy on 7/10/18.
+//  Copyright © 2018 Martin Davy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
